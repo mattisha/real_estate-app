80ec7502f9dd5e09f71d6d5a000e68d8d42ac7df
matti <matisisay71@gmail.com>
2019-10-21 22:19:22 +0300

    admin and media root updated

--- realtors/models.py
+++ realtors/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from datetime import datetime
 class Realtor(models.Model):
     name = models.CharField(max_length=200)
-    photo = models.ImageField(upload_to='photos/%y/%m/%d/',height_field=None, width_field=None, max_length=None)
+    photo = models.ImageField(upload_to='photos/%y/%m/%d/')
     description = models.TextField(blank=True, null=True)
     email = models.EmailField(max_length=254)
     phone = models.IntegerField()
     is_mvp = models.BooleanField(default=False)
-    hire_date = models.DateTimeField(datetime.now,blank=True)
+    hire_date = models.DateTimeField(default = datetime.now,blank=True)
     
 
     def __str__(self):
