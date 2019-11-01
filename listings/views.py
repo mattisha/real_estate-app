7aa184fcbdf07576ebd71981898f52b7a449612a
matti <matisisay71@gmail.com>
2019-11-01 16:20:31 +0200

    search field and sorting done

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Listing
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from listings.choices import bedroom_choices, price_choices, state_choices
 
 
 
@@ -18,8 +19,58 @@ def index(requests):
     }
     return render(requests, 'listings/listings.html',context)
 
+
+
+
+
+
 def listing(requests,listing_id):
-    return render(requests, 'listings/listing.html')
+    listings = get_object_or_404(Listing, pk=listing_id)
+    
+    context = {
+        'listings' : listings,
+    }
+    return render(requests, 'listings/listing.html', context)
+
+
+
+
+
+
 
 def search(requests):
-    return render(requests, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    if 'keywords' in requests.GET:
+        keywords = requests.GET['keywords']
+        if keywords:
+          queryset_list = queryset_list.filter(description__icontains=keywords)
+ 
+    if 'city' in requests.GET:
+        city = requests.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+
+    if 'state' in requests.GET:
+        state = requests.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(state__exact=state)
+
+    if 'bedrooms' in requests.GET:
+        bedrooms = requests.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__exact=bedrooms)
+
+    if 'price' in requests.GET:
+        price = requests.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price)
+
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'listings' : queryset_list ,
+        'values': requests.GET,
+    }
+    return render(requests, 'listings/search.html', context)
